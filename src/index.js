1973b556a7961eed666782f2d16a24dd88a008a4
Arslonbek Alimbayev <arslonbek.lion067@gmail.com>
2021-01-20 05:15:45 +0500

    react-pagination finish code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { render } from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+render(<App />, document.getElementById("root"));
+
+/*  --------- Pagination Component (24#)  ---------
+#1 -> Create pagination component (-lodash- package dan foy.)
+#2 -> Handling page changes  (active page ni ko`rsatish)
+#3 -> Paginating Data   (pagination logikasini yaratish)
+*/
